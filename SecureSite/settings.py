684981a8573d16c1510905bce35c31e2dfa83c37
Nico Sergeyssels <nico.sergeyssels@dig-x.be>
2020-12-07 20:44:56 +0100

    removed self

--- SecureSite/settings.py
+++ SecureSite/settings.py
@@ -80,7 +80,9 @@ CSP_DEFAULT_SRC = ['https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/poppe
                    "https://enigmatic-tundra-85410.herokuapp.com/static/website/style.css",
                    "https://enigmatic-tundra-85410.herokuapp.com/static/admin/js/nav_sidebar.js",
                    "'self'"]
-CSP_SCRIPT_SRC = ["'self'"]
+CSP_SCRIPT_SRC = ["https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+                  "https://www.google.com/recaptcha/api.js",
+                  "https://code.jquery.com/jquery-3.4.1.slim.min.js"]
 # CSP_REPORT_ONLY = True
 CSP_REPORT_URI = "https://fairplaydesign.report-uri.com/r/d/csp/reportOnly"
 PERMISSIONS_POLICY = {
