06dae678e4bf0d2bb27e9dbe092e692c53c7c6a9
Nico Sergeyssels <nico.sergeyssels@dig-x.be>
2020-12-08 15:21:15 +0100

    use of template explanation

--- website/views.py
+++ website/views.py
@@ -15,6 +15,12 @@ from django.views.decorators.cache import never_cache
 from django.views.generic import UpdateView
 from django.urls import reverse_lazy
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from website.tokens import account_activation_token
+
 
 @never_cache
 def welcome(request):
@@ -35,11 +41,30 @@ def signup(request):
         # form = UserCreationForm(request.POST)
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            # form.save()
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            # return redirect('/')
+
+            # verification email
+            user = form.save(commit=False)
+            user.is_active = False  # Deactivate account till it is confirmed
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activate Your SecureSite Account'
+            message = render_to_string('website/activation_email/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            messages.success(request, 'Please Confirm your email to complete registration.')
+
             return redirect('/')
     else:
         # form = UserCreationForm()
