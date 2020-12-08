66f87ce97a613051557a72fe1918a6d5e5df9fc2
Nico Sergeyssels <nico.sergeyssels@dig-x.be>
2020-12-08 14:40:53 +0100

    Profile page

--- SecureSite/urls.py
+++ SecureSite/urls.py
@@ -28,4 +28,20 @@ urlpatterns = [
     path('profile', profile, name='profile'),
     path('edit_profile/<int:pk>/', ProfileView.as_view(), name='edit_profile'),
     path('meetings/', include('meetings.urls')),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+             template_name='website/password_reset/password_reset_form.html',
+             subject_template_name='website/password_reset/password_reset_subject.txt',
+             email_template_name='commons/password-reset/password_reset_email.html',
+             # success_url='/login/'
+         ),
+         name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+             template_name='website/password_reset/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+             template_name='website/password_reset/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+             template_name='website/password_reset/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
