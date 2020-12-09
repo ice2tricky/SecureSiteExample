80b72ecdb968da1ec74e881a52688e8f31a14444
Nico Sergeyssels <nico.sergeyssels@dig-x.be>
2020-12-09 13:18:53 +0100

    update

--- meetings/urls.py
+++ meetings/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('delete/<int:id>', delete, name="delete"),
-    path('edit/<int:id>', delete, name="edit"),
+    path('edit/<int:id>', edit, name="edit"),
     path('<int:id>', detail, name="detail"),
     path('rooms', rooms, name="rooms"),
     path('new', new, name="new")
