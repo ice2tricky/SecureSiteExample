95c7001d8155bd4a236eafeac7c63e190aede34f
Nico Sergeyssels <nico.sergeyssels@dig-x.be>
2020-12-14 14:17:52 +0100

    small changes

--- SecureSite/settings.py
+++ SecureSite/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 # import django_heroku
 import os
+import environ
 
+env = environ.Env(
+)
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#)!x=lf^*2%!ye&8j+!k9&eg#^p$b#7)mx%18rtbz$36w$q0k3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
